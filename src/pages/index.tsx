83b974d4b2624b71dd32f3e0fdb6052030376012
Nico Poggi <73345016+nicolaspoggi@users.noreply.github.com>
2023-07-29 03:10:43 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home(): JSX.Element {
-  const docsPath = useBaseUrl('/docs');
+  const docsPath = useBaseUrl('/');
 
   useEffect(() => {
     window.location.href = docsPath;
